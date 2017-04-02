2e63d6f0fd2620262f3efb480c9922947fcdeaec
Camaren <camaren@Gabriels-MacBook-Pro.local>
2017-04-02 17:51:07 -0400

    YEAH

--- hello.py
+++ hello.py
@@ -3,5 +3,13 @@ def hello_world():
     print "hello world"
     return
 
-if __name__ == "__main__":
-    hello_world()
+
+def hello_earth(x):
+    return x + 3
+
+def is_even(x):
+    return x%2 == 0
+
+hello_world()
+print hello_earth(7)
+print is_even(7)
--- playtime.py
+++ playtime.py
@@ -1,6 +1,21 @@
-def great(input):
-    x = input
-    return x + 5
+def great():
 
-o = great(5)
+    l = [x for x in range(100)]
+    # initializing an empty list
+    y = []
+    # iterate the list
+    for item in l:
+        # divide by 5
+        if item%5 == 0:
+            # if remainder equals 0, then add to the list
+            y.append(item)
+    return y
 
+
+print great()
+
+
+    # divide by 2
+    # if remainder equals 0, then add to the list
+    # divide by 3
+    # if remainder equals 0, then add to the list

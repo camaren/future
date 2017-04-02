b1c7ca3752dc3acfe8e0cd67fa08494c5542b0f4
Camaren <camaren@Gabriels-MacBook-Pro.local>
2017-04-02 16:44:54 -0400

    best commitment ever

---
+++ hello.py
@@ -0,0 +1,7 @@
+# this function prints "hello world"
+def hello_world():
+    print "hello world"
+    return
+
+if __name__ == "__main__":
+    hello_world()
---
+++ playtime.py
@@ -0,0 +1,6 @@
+def great(input):
+    x = input
+    return x + 5
+
+o = great(5)
+
